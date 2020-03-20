9797707fe0d19da665bb80df919913f78246578c
WayneHsu1128 <waynecool1128@gmail.com>
2020-03-20 17:33:53 -0500

    Refactor moving direction, 提高可復用性

--- src/main/java/waynehsu/tankwar/Direction.java
+++ src/main/java/waynehsu/tankwar/Direction.java
@@ -3,20 +3,23 @@ package waynehsu.tankwar;
 import java.awt.*;
 
 enum Direction {
-    UP("U"),
-    DOWN("D"),
-    LEFT("L"),
-    RIGHT("R"),
+    UP("U", 0, -1),
+    DOWN("D", 0, 1),
+    LEFT("L", -1, 0),
+    RIGHT("R", 1, 0),
 
-    LEFT_UP("LU"),
-    RIGHT_UP("RU"),
-    RIGHT_DOWN("RD"),
-    LEFT_DOWN("LD");
+    LEFT_UP("LU", -1, -1),
+    RIGHT_UP("RU", 1, -1),
+    RIGHT_DOWN("RD", 1, 1),
+    LEFT_DOWN("LD", -1, 1);
     // 約定優於配置
     private final String abbrev;
 
-    Direction (String abbrev) {
+    final int xFactor, yFactor; // 枚舉的值,本身就不能被更改,但我們可以讓他訪問
+    Direction (String abbrev, int xFactor, int yFactor) {
         this.abbrev = abbrev;
+        this.xFactor = xFactor;
+        this.yFactor = yFactor;
     }
 
     Image getImage (String prefix) {
